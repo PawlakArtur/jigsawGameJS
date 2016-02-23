d53d1bd7877f1358686080a647edc3d772fd5cc1
Artur Pawlak <pawlak.artur90@gmail.com>
2016-02-23 19:51:17 +0100

    added random

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,37 @@
+var noNr = 9;
+start();
+moveBlock();
+
+
+function start(){
+	var ranNums = shuffle([1,2,3,4,5,6,7,8]);
+	var el = document.getElementsByClassName("field");
+	for(var i = 0; i < 8; i++){
+		el[i].innerHTML = ranNums[i];
+	}
+}
+
+function shuffle(array) {
+    var i = array.length,
+        j = 0,
+        temp;
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
 function moveBlock(){
 	var el = document.getElementsByClassName("field");
 	var i = 1;
 	var tmp 
 	[].forEach.call(el, function(field){
-		field.addEventListener("mouseenter", (function(position){
+		field.addEventListener("click", (function(position){
 			return function(){
 				checkNumber(position);
-				console.log("hi " + position);
 			}
 		})(i));
 		i++;
@@ -26,12 +51,10 @@ function checkNumber(arg){
 		if(i % 3 === 0 && i < 9){
 
 		}else{
-			console.log("+1: " + one);
 			result.push(one);
 		}
 	}
 	if(two > 0 && two < 10){
-		console.log("+3: " + two);
 		result.push(two);
 	}
 
@@ -43,68 +66,98 @@ function checkNumber(arg){
 		if(i === tmp){
 
 		}else{
-			console.log("-1: " + three);
 			result.push(three);
 		}
 	}
 
 	if(four > 0 && four < 10){
-		console.log("+3 : " + four);
 		result.push(four);
+	}
+	
+	console.log("puste pole: " + noNr);
+	var el = document.getElementsByClassName("id" + arg);
+	var el2 = document.getElementsByClassName("id" + noNr);
+	var m = [];
+	var pozycja = 0;
+	for(var il = 0; il <= result.length; il++){
+		m[il] = result[il] - arg;	
+	}
+	for(var jk = 0; jk <= m.length; jk++){
+		if(m[jk] === 1 && noNr === (arg + 1)){
+			pozycja = (el[0].offsetLeft - 1);
+			el[0].style.left = pozycja + 53 + "px";
+			console.log("ruszam w prawo");
+			setTimeout(function()
+			{ 
+				el[0].style.display = "none";
+				el2[0].style.display = "inline-block";
+				el2[0].innerHTML = el[0].innerHTML;
+				el[0].style.left = pozycja + "px";
+				el[0].innerHTML = "";
+			}, 301);
+			
+			noNr = arg;
+			console.log("puste pole: " + noNr);
+		}else if(m[jk] === -1 && noNr === (arg - 1)){
+			pozycja = (el[0].offsetLeft - 1);
+			el[0].style.left = pozycja - 53 + "px";
+			console.log("ruszam w lewo");
+			setTimeout(function()
+			{ 
+				el[0].style.display = "none";
+				el2[0].style.display = "inline-block";
+				el2[0].innerHTML = el[0].innerHTML;
+				el[0].style.left = pozycja + "px";
+				el[0].innerHTML = "";
+			}, 301);
+
+			noNr = arg;
+			console.log("puste pole: " + noNr);
+		}else if(m[jk] === 3 && noNr === (arg + 3)){
+			pozycja = (el[0].offsetTop - 1);
+			el[0].style.top = pozycja + 53 + "px";
+			console.log("ruszam w dół");
+			setTimeout(function()
+			{ 
+				el[0].style.display = "none";
+				el2[0].style.display = "inline-block";
+				el2[0].innerHTML = el[0].innerHTML;
+				el[0].style.top = pozycja + "px";
+				el[0].innerHTML = "";
+			}, 301);
+
+			noNr = arg;
+			console.log("puste pole: " + noNr);
+		}else if(m[jk] === -3 && noNr === (arg - 3)){
+			pozycja = (el[0].offsetTop - 1);
+			el[0].style.top = pozycja - 53 + "px";
+			console.log("ruszam w górę");
+			setTimeout(function()
+			{ 
+				el[0].style.display = "none";
+				el2[0].style.display = "inline-block";
+				el2[0].innerHTML = el[0].innerHTML;
+				el[0].style.top = pozycja + "px";
+				el[0].innerHTML = "";
+			}, 301);
+
+			noNr = arg;
+			console.log("puste pole: " + noNr);
+		}
 	}
 }
 
-moveBlock();
-// check();
-
-// function check(){
-// 	var i = 1;
-// 	var j = 1;
-// 	var k = 1;
-// 	var l = 1;
-// 	var wyniki = 0;
-// 	var wynikj = 0;
-// 	var wynikk = 0;
-// 	var wynikl = 0;
-// 	var tmp = 0;
-// 	for(var a = 0; a < 9; a++){
-// 		if(i % 3 === 0 && i < 9){
-// 			wyniki = 0;
-// 		} else{
-// 			wyniki = i + 1;
-// 		}
-// 		wyniki = i + 1;
-// 		wynikj = j + 3;
-// 		if(wyniki < 10){
-// 			if(i % 3 === 0 && i < 9){
-
-// 			}else{
-// 				console.log("'+1' " + i + " " + wyniki);
-// 			}
-// 		}
-// 		if(wynikj < 10){
-// 			console.log("'+3' " + j + " " + wynikj);
-// 		}
-// 		i++;
-// 		j++;
-
-// 		wynikk = k - 1;
-// 		wynikl = l - 3;
-
-// 		if(k % 3 === 0 && k < 9){
-// 			tmp = k + 1;
-// 		}
-// 		if(wynikk > 0){
-// 			if(k === tmp){
-
-// 			}else{
-// 				console.log("'-1' " + k + " " + wynikk);
-// 			}
-// 		}
-// 		if(wynikl > 0){
-// 			console.log("'-3' " + l + " " + wynikl);
-// 		}
-// 		k++;
-// 		l++;
-// 	}
-// }
+function check(){
+	var el = document.getElementsByClassName("field");
+	var tab = ["1", "2", "3", "4", "5", "6", "7", "8"];
+	var tmp = [];
+	for(var i = 0; i < 8; i++){
+		tmp[i] = el[i].innerHTML;
+	}
+	if(tab.toString() === tmp.toString()){
+		alert("Wygrana");
+		console.log("równe");
+	}else{
+		console.log("nierówne");
+	}
+}
